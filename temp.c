325f73a9882dd1a046333bcbe28839d4d1e8d483
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2024-08-26 15:04:12 +0530

    "Updated P9.txt with new SQL queries and modified temp.c with new string insertion logic"

--- temp.c
+++ temp.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    int ind;
     int i = 0;
-start:
-    i++;
-    printf("%d\n", i);
-    goto start;
+    int j = 0;
+    int k = 0;
+    char a[100];
+    char b[100];
+    char c[100];
+    printf("enter the string :");
+    gets(a);
+    printf("enter the new string  insert into 1 st string :");
+    gets(b);
+    printf("enter position insert into string :");
+    scanf("%d", &ind);
+    while (i < ind && a[i] != '\0')
+    {
+        i++;
+        c[k] = a[i];
+        k++;
+    }
+    while (b[j] != '\0')
+    {
+        c[k] = b[j];
+        j++;
+        k++;
+    }
+    while (a[i] != '\0')
+    {
+        c[k] = a[i];
+        i++;
+        k++;
+    }
+    c[k] = '\0';
+    printf("new string is %s", c);
     return 0;
 }
