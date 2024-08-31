dc957a2f9bbb9e69e12d474e827fe20511c3ad87
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2024-08-31 15:49:21 +0530

    "Removed duplicate enqueue and dequeue operations in main function."

--- DSA/code/circular_queue.c
+++ DSA/code/circular_queue.c
@@ -11,62 +11,66 @@ struct queue
 
 void print_queue(struct queue *q)
 {
-    if (q->front == -1 || q->front > q->rear)
+    if (q->front == -1)
     {
-        printf("Queue is empty cannot print\n");
+        printf("Queue is empty, cannot print\n");
         return;
     }
 
-    int front = q->front;
-    int rear = q->rear;
-    while (front <= rear)
+    int i = q->front;
+    while (1)
     {
-        printf("%d ", q->arr[front]);
-        front++;
+        printf("%d ", q->arr[i]);
+        if (i == q->rear)
+            break;
+        i = (i + 1) % q->size;
     }
     printf("\n");
 }
 
 void enqueue(struct queue *q, int value)
 {
-    if ((q->rear == -1) && (q->front == -1))
+    if ((q->rear + 1) % q->size == q->front)
+    {
+        printf("Queue is full\n");
+        return;
+    }
+
+    if (q->front == -1)
     {
         q->front = 0;
         q->rear = 0;
         q->arr[q->rear] = value;
     }
-    else if (q->rear != (q->size - 1))
-    {
-        q->rear++;
-        q->arr[q->rear] = value;
-    }
     else
     {
-        printf("Queue is full\n");
+        q->rear = (q->rear + 1) % q->size;
+        q->arr[q->rear] = value;
     }
 }
 
 void dequeue(struct queue *q)
 {
-    if (q->front == -1 || q->front > q->rear)
+    if (q->front == -1)
     {
         printf("Queue is empty\n");
         return;
     }
-    else if (q->front == q->rear)
-    {
+
+    if (q->front == q->rear)
+    { // Queue has only one element
         q->front = -1;
         q->rear = -1;
     }
     else
     {
-        q->front++;
+        q->front = (q->front + 1) % q->size;
     }
 }
 
 int main()
 {
-    struct queue *q;
+    struct queue *q = (struct queue *)malloc(sizeof(struct queue));
     q->size = 10;
     q->arr = (int *)malloc(q->size * sizeof(int));
     q->front = -1;
@@ -80,7 +84,8 @@ int main()
     print_queue(q);
     dequeue(q);
     dequeue(q);
-    dequeue(q);
+    enqueue(q, 60);
+    enqueue(q, 70);
     print_queue(q);
 
     return 0;
