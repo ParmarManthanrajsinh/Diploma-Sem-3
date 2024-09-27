91dc8a14efa71b5de2b6dbf4a67dd30042209863
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2024-09-27 23:56:05 +0530

    added unit 4 and 5 in DSA

---
+++ Python/Practice/plot.py
@@ -0,0 +1,45 @@
+"""
+Before running the program
+you need to install matplotlib
+
+you can install it by running the following command in your terminal:
+pip install matplotlib
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# xpoint = [5, 10, 18]
+# ypoint = [1, 2, 20]
+# plt.ylabel("y axis")
+# plt.xlabel("x axis")
+# plt.plot(xpoint, ypoint, marker='x')
+# plt.grid()
+# plt.show()
+
+# eno = np.array([1, 2, 3, 4])
+# mark = np.array([10, 12, 20, 15])
+# # mark = eno * 2
+# plt.ylabel("y axis")
+# plt.xlabel("x axis")
+# plt.bar(eno, mark)
+# plt.show()
+
+# x = np.arange(0, 2 * np.pi, 0.1)
+# y = np.sin(x)
+# print(x)
+# plt.plot(x, y)
+# plt.show()
+
+# Create a figure with 2 rows and 3 columns
+# plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+# plt.subplot(2, 3, 1)
+# plt.plot(x, y)
+# # plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+# plt.subplot(2, 3, 6)
+# plt.plot(x, y)
+# plt.show()
