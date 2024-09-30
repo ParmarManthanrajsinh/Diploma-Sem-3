e7d8c27ee00cb051327dc62fe1412932395c6731
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2024-09-30 12:45:00 +0530

    "Updated q2.py to read a string and count vowels and consonants, and modified q4.py to change histogram settings."

---
+++ Python/code/P10/q4.py
@@ -0,0 +1,4 @@
+# Write  a  program  that  checks  whether  two  words  are  anagrams,  Two  words  are 
+# anagrams  if  they  contain  the  same  letters.  For  example,  silent  and  listen  are 
+# anagrams. 
+
--- Python/code/P9/q4.py
+++ Python/code/P9/q4.py
@@ -2,12 +2,11 @@
 # # where each element represents a student's marks (between 0 to 10), and display a 
 # # histogram for the result. 
 
-import numpy as np
 import matplotlib.pyplot as plt
 
 Result = [1, 1, 8, 1, 10, 1, 1, 8, 10, 10, 10, 3, 3, 8, 4, 9, 8, 5, 4, 9, 6, 8, 5, 7, 3, 7, 1, 8, 7, 8, 9, 6, 2, 1, 8, 5, 2, 1, 2, 4, 10, 7, 4, 3, 8, 9, 5, 10, 1, 4, 1, 8, 9, 2, 10, 3, 4, 8, 4, 6, 2, 7, 10, 10, 8, 8, 2, 2, 3, 10, 7, 6, 3, 8, 2, 9, 9, 8, 7, 9, 1, 9, 8, 7, 3, 2, 9, 8, 7, 10, 10, 6, 6, 6, 7, 3, 3, 3, 4, 2, 1]
 
-plt.hist(Result, bins=20, color='red', edgecolor='black',range=(0,10))
+plt.hist(Result, bins=30, range=(0,10))
 
 plt.title('Result')
 plt.xlabel('X')
