5b34ba1b05c9394aad9ba2bdc2b3e5ed568f092e
Dhaval572 <dp9718776@gmail.com>
2024-10-02 19:00:21 +0530

    Update q4.py

--- Python/code/P10/q4.py
+++ Python/code/P10/q4.py
@@ -1,4 +1,16 @@
 # Write  a  program  that  checks  whether  two  words  are  anagrams,  Two  words  are 
-# anagrams  if  they  contain  the  same  letters.  For  example,  silent  and  listen  are 
-# anagrams. 
+# anagrams  if  they  contain  the  same  letters.  For  example,  silent  and  listen  are anagrams.
 
+def are_anagrams(word1, word2):
+
+    word1 = word1.replace(" ", "").lower()
+    word2 = word2.replace(" ", "").lower()
+    return sorted(word1) == sorted(word2)
+
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+if are_anagrams(word1, word2):
+    print(f'"{word1}" and "{word2}" are anagrams.')
+else:
+    print(f'"{word1}" and "{word2}" are not anagrams.')
